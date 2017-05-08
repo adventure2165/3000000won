7375fca73fbfb42d5cd4f8273b51c1d46489ef23
KimSeungYoon <KimSeungYoon@DESKTOP-2S6SCFU>
2017-05-09 00:29:06 +0900

    Build basic structure.

--- Connect6Algo.h
+++ Connect6Algo.h
@@ -7,6 +7,18 @@ extern int width, height;
 extern int terminateAI;
 extern unsigned s_time;
 
+//we can change any type.
+typedef int estimate_type;
+
+//State Node
+extern class Node {
+private:
+public:
+	//We might delete this data;
+	Node* childs;
+	bool isTerminalNode();
+};
+
 extern int isFree(int x, int y);
 extern int showBoard(int x, int y);
 extern void init();
@@ -16,6 +28,9 @@ extern void opmove(int x[], int y[], int cnt = 2);
 extern void block(int, int);
 extern int setLine(char *fmt, ...);
 extern void domymove(int x[], int y[], int cnt = 2);
+
+extern estimate_type alphabeta_pruning(Node state, int depth, estimate_type alpha, estimate_type beta, bool player);
+extern estimate_type estimate_function(Node state);
 
 void myturn(int cnt = 2);
 char info[];
