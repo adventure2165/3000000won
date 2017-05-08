753dec9dc90d857ae47327b2792caf0fc0ec08c9
KimSeungYoon <KimSeungYoon@DESKTOP-2S6SCFU>
2017-05-09 00:29:06 +0900

    Build basic structure.

--- AI_Algorithm_Code.cpp
+++ AI_Algorithm_Code.cpp
@@ -36,34 +36,79 @@ int showBoard(int x, int y) : [x, y] 좌표에 무슨 돌이 존재하는지 보여주는 함수 (
 #include <stdio.h>
 #include <Windows.h>
 #include <time.h>
+#include <cmath>
 #include "Connect6Algo.h"
 
 // "샘플코드[C]"  -> 자신의 팀명 (수정)
 // "AI부서[C]"  -> 자신의 소속 (수정)
 // 제출시 실행파일은 반드시 팀명으로 제출!
 char info[] = { "TeamName:300만원,Department:고려대학교" };
+int depth = 4;
+estimate_type result;
 
 void myturn(int cnt) {
 
 	int x[2], y[2];
 
-	// 이 부분에서 알고리즘 프로그램(AI)을 작성하십시오. 기본 제공된 코드를 수정 또는 삭제하고 본인이 코드를 사용하시면 됩니다.
-	// 현재 Sample code의 AI는 Random으로 돌을 놓는 Algorithm이 작성되어 있습니다.
+	//fist
+	if (cnt == 1) {
+		result = alphabeta_pruning(current, depth, INFINITE * -1, INFINITE, TRUE);
 
-	srand((unsigned)time(NULL));
+		x[0] = 0;
+		y[0] = 0;
+	}
+	//otherwise
+	else {
+		for (int i = 0; i < cnt; ++i) {
+			result = alphabeta_pruning(current, depth, INFINITE * -1, INFINITE, TRUE);
 
-	for (int i = 0; i < cnt; i++) {
-		do {
-			x[i] = rand() % width;
-			y[i] = rand() % height;
-			if (terminateAI) return;
-		} while (!isFree(x[i], y[i]));
-
-		if (x[1] == x[0] && y[1] == y[0]) i--;
+			x[i] = 0;
+			y[i] = 0;
+		}
 	}
 
-	// 이 부분에서 자신이 놓을 돌을 출력하십시오.
-	// 필수 함수 : domymove(x배열,y배열,배열크기)
-	// 여기서 배열크기(cnt)는 myturn()의 파라미터 cnt를 그대로 넣어야합니다.
 	domymove(x, y, cnt);
+}
+
+estimate_type alphabeta_pruning(Node state, int depth, estimate_type alpha, estimate_type beta, bool player)
+{
+	estimate_type v;
+
+	if (depth == 0 || state.isTerminalNode()) {
+		return estimate_function(state);
+	}
+
+	if (player) {
+		v = INFINITE * -1;
+
+		for each(Node child in state.childs) {
+			v = max(v, alphabeta_pruning(child, depth - 1, alpha, beta, FALSE));
+			alpha = max(alpha, v);
+
+			if (beta <= alpha)
+				break;
+		}
+
+		return v;
+	}
+	else {
+		v = INFINITE;
+
+		for each(Node child in state.childs) {
+			v = min(v, alphabeta_pruning(child, depth - 1, alpha, beta, TRUE));
+			beta = min(beta, v);
+
+			if (beta <= alpha)
+				break;
+		}
+
+		return v;
+	}
+}
+
+estimate_type estimate_function(Node state)
+{
+	estimate_type estimate_value;
+
+	return estimate_value;
 }
